574d101117f159f3859f3edddff536d59aebc531
Jigaracharya <jigaracharya@gmail.com>
2021-02-21 12:40:11 -0600

    first version of  library committed

---
+++ Security/EncryptionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Security
+{
+    public class EncryptionHelper
+    {
+        public string Encrypt(string encrypt)
+        {
+            return $"Encrypt {encrypt}";
+        }
+
+        public string Dencrypt(string dencrypt)
+        {
+            return $"Dencrypt {dencrypt}";
+        }
+    }
+}
